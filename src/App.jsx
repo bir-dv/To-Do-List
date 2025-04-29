a5d58dc10653c40910c6ce80899ea42c104907fc
bir-dv <brukjoseph@gmail.com>
2025-04-29 15:54:24 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Typography variant="h4" gutterBottom>
+        Welcome
+      </Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
